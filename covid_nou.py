79b6228100b521001e5aea427dbdd7576255b142
Razvan Avesalon <89531556+RazvanAVESALON@users.noreply.github.com>
2021-09-30 15:52:28 +0300

    Update covid_nou.py

--- covid_nou.py
+++ covid_nou.py
@@ -1,1 +1,10 @@
+import tensorflow as tf
+from tensorflow.keras import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Conv2D
+from tensorflow.keras.layers import MaxPool2D
+from tensorflow.keras.layers import Flatten
+from tensorflow.keras.layers import Dropout
 
+from numpy import unique
+from numpy import argmax
