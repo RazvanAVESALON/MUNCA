1ce623b1ea1ef21f2a5d9c6ce23dfc83ea29097e
Razvan Avesalon <89531556+RazvanAVESALON@users.noreply.github.com>
2021-10-25 14:00:10 +0300

    Update covid_nou.py

--- covid_nou.py
+++ covid_nou.py
@@ -57,14 +57,7 @@ BATCH_SIZE = config['train']['bs']
 
 
 
-train_datagen = ImageDataGenerator( rescale=1./255,
-                                    rotation_range=40,
-                                    width_shift_range=0.2,
-                                    height_shift_range=0.2,
-                                    shear_range=0.2,
-                                    zoom_range=0.2,
-                                    horizontal_flip=True,
-                                    fill_mode='nearest')
+train_datagen = ImageDataGenerator(**config['augumentare'])
 validation_datagen = ImageDataGenerator(rescale=1./255)     
 
 train_batches = train_datagen.flow_from_directory(dataset_dir + '/train',
